c33ef29e8156b8b435ef2ba0cdbc1824474db1f0
Ata-ur-Rahman <ataurrehman56@gmail.com>
2019-09-23 20:30:32 +0500

    Auth basic setup with firebase

---
+++ auth-burger-builder/src/store/actions/auth.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+import * as actionTypes from './actionTypes';
+
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START,
+  };
+};
+
+export const authSuccess = authData => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    authData,
+  };
+};
+
+export const authFail = error => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: error,
+  };
+};
+
+export const auth = (email, password, isSignup) => {
+  return dispatch => {
+    dispatch(authStart());
+    const authData = {
+      email,
+      password,
+      returnSecureToken: true,
+    };
+    axios
+      .post(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAUJ-gnuo3dM_FHRveVDwcAQFyyGAcRvtI',
+        authData,
+      )
+      .then(response => {
+        dispatch(authSuccess(response.data));
+      })
+      .catch(error => {
+        dispatch(authFail(error));
+      });
+  };
+};
